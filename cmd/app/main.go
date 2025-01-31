e9a88cad27d38be9cca562cfb376e4e9db3481c8
Gabriel Freitas <gabriielfs96@gmail.com>
2025-01-31 15:18:38 -0300

    chore: added initial configs for database and server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,16 +3,34 @@ package main
 import (
 	"log"
 
-	"github.com/FreitasGabriel/red-fox-challenge/internal/routes"
+	"github.com/FreitasGabriel/red-fox-challenge/config"
+	"github.com/FreitasGabriel/red-fox-challenge/config/database/connection"
+	"github.com/FreitasGabriel/red-fox-challenge/config/dependencies"
+	"github.com/FreitasGabriel/red-fox-challenge/config/logger"
+	"github.com/FreitasGabriel/red-fox-challenge/internal/api/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	c := gin.Default()
 
-	routes.InitRoutes(&c.RouterGroup)
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		logger.Error("error to initialize the env configs", err)
+		return
+	}
 
-	err := c.Run(":8080")
+	db, err := connection.NewConnectionDatabase(config)
+	if err != nil {
+		logger.Error("error to get connection database", err)
+		return
+	}
+
+	handler := dependencies.InitDependencies(db)
+
+	routes.InitRoutes(&c.RouterGroup, handler)
+
+	err = c.Run(":8080")
 	if err != nil {
 		log.Fatal("error to start server")
 	}
